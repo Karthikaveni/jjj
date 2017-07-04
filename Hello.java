85db335ee6839bcba6d08e98601aab27228f2363
Karthikaveni <karthikaveni18@gmail.com>
2017-07-04 10:18:37 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+# jjj
+import java.io.*;
+import java.util.*;
+
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    for(int i = 0; i < 5; i++) {
+      System.out.println("Hello");
+    }
+  }
+  }
